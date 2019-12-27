b9c81b948f23082efbba98acadf8c8d9af157d41
Aitor <aitorpalomares@hotmail.com>
2019-12-27 08:31:37 -0300

    rca with storybook

---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+const Button: React.FC = () => {
+  return (
+    <button>
+      bla bla
+    </button>
+  );
+}
+
+export { Button };
