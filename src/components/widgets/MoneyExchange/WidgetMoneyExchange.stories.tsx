c199019b20345266f96597a23b873b8132d2d888
Aitor <aitorpalomares@hotmail.com>
2020-01-07 09:51:11 +0000

    story for widgets

---
+++ src/components/widgets/MoneyExchange/WidgetMoneyExchange.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { WidgetMoneyExchange } from './WidgetMoneyExchange';
+
+export default {
+  title: `Widgets|MoneyExchange`,
+};
+
+export const basic = () => <WidgetMoneyExchange />;
